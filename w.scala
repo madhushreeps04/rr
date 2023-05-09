7b8d38fc26ec095499b4a712e0c83e21560a0845
madhushreeps04 <95095205+madhushreeps04@users.noreply.github.com>
2023-05-09 12:34:13 +0530

    Add files via upload

---
+++ w.scala
@@ -0,0 +1,21 @@
+import scala.io.Source
+object wordcount2{
+ 	def main(args:Array[String]){
+ 	if(args.length!=1){
+ 	System.err.println("error")
+ 	System.exit(1)
+ }
+ 
+ var filename=args(0)
+ val wordC=scala.collection.mutable.Map[String,Int]()
+ for(line<-Source.fromFile(filename).getLines)
+ for(word<-line.split(" "))
+
+ wordC(word)=if(wordC.contains(word)) wordC(word)+1 else 1
+ println(wordC)
+ for((k,v)<-wordC)
+ printf("word %s occurs %d",k,v)
+ }
+ }
+
+ 	
